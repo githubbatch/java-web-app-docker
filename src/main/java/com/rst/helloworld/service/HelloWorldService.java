b5022dfdb0654a003938fc618a2c4b413f13b1df
saiteja <139723705+saiteja-0907@users.noreply.github.com>
2023-10-26 19:08:03 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hello mamatha";
+			return "Hello Docker";
 		}else{
 			return "Hello " + name;
 		}
